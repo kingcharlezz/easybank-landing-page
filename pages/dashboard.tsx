f196362208f7e581ffeb14d2cf7397338f81cf05
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2023-06-22 21:04:26 -0400

    recommiot idk

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,6 +4,8 @@ import Example from '../components/youtube/page';
 import { UserContext } from './_app';
 import { getAuth, signOut } from 'firebase/auth';
 import { FaUsers, FaDollarSign, FaBoxOpen, FaShoppingCart } from 'react-icons/fa';
+import ReactMarkdown from "react-markdown";
+
 
 interface DashboardProps {
   children?: ReactNode;
@@ -43,15 +45,25 @@ export default function Dashboard({ children }: DashboardProps) {
   const [darkMode, setDarkMode] = useState<boolean>(false);
   
   const toggleDarkMode = () => {
+    if (!darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
     setDarkMode(!darkMode);
   };
   
   return (
-    <div className={`${darkMode ? 'bg-dark-blue text-neutral-white' : 'bg-neutral-very-light-gray text-primary-black'} h-screen flex`}>
-      <aside className={`${darkMode ? 'bg-dark-gray' : 'bg-neutral-light-grayish-blue'} shadow w-64 min-h-screen p-6 fixed top-0 left-180`}>
+    <><meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+    <div className={`${
+    darkMode ? 'bg-dark-blue text-neutral-white' : 'bg-neutral-very-light-gray text-primary-black'
+  } min-h-screen grid grid-cols-[16rem,1fr]`}>
+  <aside className={`${
+    darkMode ? 'bg-dark-gray' : 'bg-neutral-light-grayish-blue'
+  } fixed shadow w-64 min-h-screen p-6`}>
         <h1 className={`${darkMode ? 'text-neutral-white' : 'text-primary-black'} text-2xl font-sans font-bold mb-6`}>Your Dashboard</h1>
         <nav>
-          <a 
+          <a
             href="#"
             onClick={() => setView('noteTaking')}
             className={`${darkMode ? 'text-neutral-white border-neutral-white' : 'text-primary-black border-primary-black'} mb-4 border-2 hover:text-dark-blue rounded p-3 transition-colors duration-200 block`}
@@ -69,23 +81,31 @@ export default function Dashboard({ children }: DashboardProps) {
           </a>
         </nav>
       </aside>
-      <main className={`${darkMode ? 'bg-darker-blue' : 'bg-neutral-very-light-gray'} flex-grow mx-auto py-10 px-6 ml-64`}>
-        <header className={`${darkMode ? 'bg-dark-gray' : 'bg-neutral-light-grayish-blue'} flex items-center justify-between p-5 rounded-md mb-10`}>
-          <h2 className="text-3xl font-sans font-bold">!</h2>
-          <div className="flex items-center justify-end">
-            <button 
-              onClick={toggleDarkMode} 
-              className={`mr-2 ${darkMode ? 'bg-darker-blue text-neutral-white border-neutral-white' : 'bg-darker-blue text-neutral-white border-primary-black'} rounded-md px-4 py-2`}
-            >
-              {darkMode ? 'Light Mode' : 'Dark Mode'}
-            </button>
-            <button className="bg-darker-blue text-white border-primary-black border-2 rounded-md px-4 py-2">
-              Logout
-            </button>
+      <div style={{ marginLeft: '16rem' }}> {/* Use style prop to add margin-left */}
+        <main className={`${darkMode ? 'bg-darker-blue' : 'bg-neutral-very-light-gray'} w-[calc(100%-16rem)] py-10 px-6 flex flex-col`}>
+          <header className={`${darkMode ? 'bg-dark-gray' : 'bg-neutral-light-grayish-blue'} flex items-center justify-between p-5 rounded-md mb-10`}>
+            <h2 className="text-3xl font-sans font-bold">Welcome back!</h2>
+            <div className="flex items-center justify-end">
+              <button
+                onClick={toggleDarkMode}
+                className={`mr-2 ${darkMode ? 'bg-darker-blue text-neutral-white border-neutral-white' : 'bg-darker-blue text-neutral-white border-primary-black'} rounded-md px-4 py-2`}
+              >
+                {darkMode ? 'Light Mode' : 'Dark Mode'}
+              </button>
+              <button
+                onClick={handleLogout}
+                className="bg-darker-blue text-white border-primary-black border-2 rounded-md px-4 py-2"
+              >
+                Logout
+              </button>
+            </div>
+          </header>
+          <div className="w-full text-center">
+            {renderView()}
           </div>
-        </header>
-        {renderView()}
-      </main>
-    </div>
-  );
-};
+        </main>
+      </div>
+    </div></>
+    );
+
+  }
